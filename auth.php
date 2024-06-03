d61ec78211672a96ea5d73d1bc203969184c623e
nitins10b <nitins10b@gmail.com>
2024-06-03 17:33:48 +0530

    php

---
+++ auth.php
@@ -0,0 +1,10 @@
+<?php
+if(session_status() === PHP_SESSION_NONE)
+session_start();
+$link = $_SERVER['PHP_SELF'];
+if(!strpos($link,'login.php') && !strpos($link,'login_verification.php') && !strpos($link,'registration.php') && !isset($_SESSION['user_login'])){
+echo "<script>location.replace('./login.php');</script>";
+}
+if(strpos($link,'login_verification.php') && !isset($_SESSION['otp_verify_user_id'])){
+    echo "<script>location.replace('./login.php');</script>";
+}
